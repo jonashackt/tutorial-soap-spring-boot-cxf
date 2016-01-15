28a0ae1613bf4bf05518991545ac9833ece7e36a
Jonas Hecht <jonas.hecht@codecentric.de>
2016-01-15 09:45:56 +0100

    Even simpler

--- step1_simple_springboot_app_with_cxf/src/main/java/de/jonashackt/tutorial/SimpleBootCxfApplication.java
+++ step1_simple_springboot_app_with_cxf/src/main/java/de/jonashackt/tutorial/SimpleBootCxfApplication.java
@@ -1,7 +1,12 @@
 package de.jonashackt.tutorial;
 
+import org.apache.cxf.Bus;
+import org.apache.cxf.bus.spring.SpringBus;
+import org.apache.cxf.transport.servlet.CXFServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SimpleBootCxfApplication {
@@ -9,4 +14,14 @@ public class SimpleBootCxfApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleBootCxfApplication.class, args);
 	}
+	
+	@Bean
+    public ServletRegistrationBean dispatcherServlet() {
+        return new ServletRegistrationBean(new CXFServlet(), "/soap-api/*");
+    }
+    
+    @Bean(name=Bus.DEFAULT_BUS_ID)
+    public SpringBus springBus() {      
+        return new SpringBus();
+    }
 }
